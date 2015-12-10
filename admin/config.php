37a5f7e1716488b896dd43db8f3b1292058ef588
due <trantridue@gmail.com>
2015-12-10 17:57:24 +0700

    commmit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/2nine/admin/');
+define('HTTP_CATALOG', 'http://localhost/2nine/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/2nine/admin/');
+define('HTTPS_CATALOG', 'http://localhost/2nine/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/2nine/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/2nine/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/2nine/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/2nine/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/2nine/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/2nine/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/2nine/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/2nine/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/2nine/system/upload/');
+define('DIR_LOGS', 'C:/xampp/htdocs/2nine/system/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/2nine/system/modification/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/2nine/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aintmtdo_2nine');
+define('DB_PASSWORD', 'r89dR8T2nu');
+define('DB_DATABASE', 'aintmtdo_2nine');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '2nine_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/2nine/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/2nine/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/2nine/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/2nine/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/2nine/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/2nine/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/2nine/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/2nine/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/2nine/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/2nine/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/2nine/system/upload/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/2nine/system/modification/');
+define('DIR_LOGS', 'C:/xampp/htdocs/2nine/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aintmtdo_2nine');
+define('DB_PASSWORD', 'r89dR8T2nu');
+define('DB_DATABASE', 'aintmtdo_2nine');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '2nine_');
