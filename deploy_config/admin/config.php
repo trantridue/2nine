b2b0728897ff14edef21fc4cd811da1c8ea50c16
due <trantridue@gmail.com>
2015-12-10 18:19:50 +0700

    dd

---
+++ deploy_config/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://2nine.vn/admin/');
+define('HTTP_CATALOG', 'http://2nine.vn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://2nine.vn/admin/');
+define('HTTPS_CATALOG', 'http://2nine.vn/');
+
+// DIR
+define('DIR_APPLICATION', '/home/aintmtdo/public_html/admin/');
+define('DIR_SYSTEM', '/home/aintmtdo/public_html/system/');
+define('DIR_LANGUAGE', '/home/aintmtdo/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/aintmtdo/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/aintmtdo/public_html/system/config/');
+define('DIR_IMAGE', '/home/aintmtdo/public_html/image/');
+define('DIR_CACHE', '/home/aintmtdo/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/aintmtdo/public_html/system/download/');
+define('DIR_UPLOAD', '/home/aintmtdo/public_html/system/upload/');
+define('DIR_LOGS', '/home/aintmtdo/public_html/system/logs/');
+define('DIR_MODIFICATION', '/home/aintmtdo/public_html/system/modification/');
+define('DIR_CATALOG', '/home/aintmtdo/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aintmtdo_2nine');
+define('DB_PASSWORD', 'r89dR8T2nu');
+define('DB_DATABASE', 'aintmtdo_2nine');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '2nine_');
